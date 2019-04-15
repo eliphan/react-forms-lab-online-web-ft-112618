a2b6942e54862e29aa2aeffed943e5cf4632dbe3
Elise Phan <aliceconnic@gmail.com>
2019-04-15 00:29:57 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -11,9 +11,9 @@ class LoginForm extends React.Component {
   }
   
   handleChange = (event) => {
-  this.setState({
-    [event.target.name]: event.target.value
-  })
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   
   handleSubmit = event => {
