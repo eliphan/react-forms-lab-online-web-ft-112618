1ab8e911674f3ee9a32205bc27493d753d08b216
Elise Phan <aliceconnic@gmail.com>
2019-04-15 00:18:59 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -25,7 +25,7 @@ class TwitterMessage extends React.Component {
                  value={this.state.value}
                  onChange={this.handleChange}/>
         </form>
-        {this.props.maxChars-this.state.message.length}
+        {this.props.maxChars-this.state.value.length}
       </div>
     );
   }
